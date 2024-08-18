e34a2552ec07c181264677d040ed0d6f93600437
Anisha Nayaju <anishanayaju26@gmail.com>
2024-08-18 19:07:34 +0545

    overview

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
+import Header from "../components/common/Header";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex-1  overflow-auto relative z-10">
+      <Header title="Overview" />
+    </div>
+  );
 };
 
 export default HomePage;
