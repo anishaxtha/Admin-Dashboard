82258c89dccc2540464ccb266e36b2d2699e467d
Anisha Nayaju <anishanayaju26@gmail.com>
2024-08-18 15:11:10 +0545

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import Sidebar from "./components/Sidebar";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div
+          className="absolute bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80
+        "
+        ></div>
+        <div className="absolute inset-0 backdrop-blur-sm"></div>
+      </div>
+
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </div>
   );
 };
