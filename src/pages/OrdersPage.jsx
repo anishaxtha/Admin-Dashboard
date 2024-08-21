723fa3caab09c4646479582e0c44b76aba3dc97b
Anisha Nayaju <anishanayaju26@gmail.com>
2024-08-21 14:58:03 +0545

    order page

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "../components/common/Header";
+import { motion } from "framer-motion";
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+  return (
+    <div className="flex-1  overflow-auto relative  z-10">
+      <Header title="Orders" />
+      <motion.div
+        className="grid grid-cols-1 gap-5  sm:grid-cols-2 lg:grid-cols-4  mb-8"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duation: 1 }}
+      ></motion.div>
+    </div>
+  );
 };
 
 export default OrdersPage;
