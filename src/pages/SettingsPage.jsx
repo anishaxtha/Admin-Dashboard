cbc9f419cc8fdae91fb4a612ada15d9589ee8dd3
Anisha Nayaju <anishanayaju26@gmail.com>
2024-08-24 17:05:31 +0545

    settingspage

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Header from "../components/common/Header";
 import ProfileSection from "../components/settings/ProfileSection";
 import Notifications from "../components/settings/Notifications";
+import Security from "../components/settings/Security";
+import ConnectedAccounts from "../components/settings/ConnectedAccounts";
+import DangerZone from "../components/settings/DangerZone";
 
 const SettingsPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Settings" />
-      <main className="max-w-4xl mx-32 py-6 px-4 lg:px-8">
+      <main className="max-w-4xl mx-16 py-6 px-4 lg:px-8">
         <ProfileSection />
         <Notifications />
+        <Security />
+        <ConnectedAccounts />
+        <DangerZone />
       </main>
     </div>
   );
