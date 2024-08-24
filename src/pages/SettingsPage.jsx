515f82b2ca4b2d728cc1edad5c681917d3e52540
Anisha Nayaju <anishanayaju26@gmail.com>
2024-08-24 10:31:05 +0545

    settingpage

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../components/common/Header";
+import ProfileSection from "../components/settings/ProfileSection";
 
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Settings" />
+      <main className="max-w-4xl mx-32 py-6 px-4 lg:px-8">
+        <ProfileSection />
+      </main>
+    </div>
+  );
 };
 
 export default SettingsPage;
