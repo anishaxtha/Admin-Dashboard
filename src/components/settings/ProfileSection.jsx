04b18707b4c7657f54adf6a7f1e53714e9076e58
Anisha Nayaju <anishanayaju26@gmail.com>
2024-08-24 10:30:59 +0545

    profile

---
+++ src/components/settings/ProfileSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import SettingSection from "./SettingSection";
+import { FaRegUser } from "react-icons/fa";
+
+const ProfileSection = () => {
+  return (
+    <SettingSection icon={FaRegUser} title="Profile">
+      <div className="flex flex-col sm:flex-row items-center  mb-6">
+        <img
+          src="https://randomuser.me/api/portraits/women/5.jpg"
+          alt="Profile"
+          className="rounded-full  size-20 object-cover mr-4 "
+        />
+
+        <div>
+          <h3 className="text-lg font-semibold text-gray-100">
+            Heenaa Johnson
+          </h3>
+          <p className="text-gray-400">heena.johnson@example.com</p>
+        </div>
+      </div>
+      <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
+        Edit Profile
+      </button>
+    </SettingSection>
+  );
+};
+
+export default ProfileSection;
